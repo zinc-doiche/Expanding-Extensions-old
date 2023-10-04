f0bad5f45718aebf0477f6e9be67cf81f212f802
dev-zinc <zchan1325@gmail.com>
2023-10-04 16:14:32 +0900

    Feat: Structure spawn event handler

--- src/main/kotlin/com/github/zinc/module/world/listener/WorldListener.kt
+++ src/main/kotlin/com/github/zinc/module/world/listener/WorldListener.kt
@@ -8,9 +8,12 @@ import kotlinx.coroutines.delay
 import org.bukkit.Material
 import org.bukkit.World
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerChangedWorldEvent
 import org.bukkit.event.player.PlayerItemConsumeEvent
+import org.bukkit.event.world.AsyncStructureSpawnEvent
+import org.bukkit.event.world.LootGenerateEvent
 import org.bukkit.potion.PotionEffect
 import org.bukkit.potion.PotionEffectType
 
@@ -52,5 +55,24 @@ class WorldListener: Listener {
         if(event.player.world.environment == World.Environment.NETHER) {
             event.isCancelled = true
         }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    fun onAsyncStructureSpawn(event: AsyncStructureSpawnEvent) {
+        with(event.boundingBox) {
+            for(x in minX.toInt()..maxX.toInt()) {
+                for(y in minY.toInt()..maxY.toInt()) {
+                    for(z in minZ.toInt()..maxZ.toInt()) {
+                        val block = event.world.getBlockAt(x, y, z)
+                        if(block.type == Material.CHEST
+                            || block.type == Material.CHEST_MINECART
+                            || block.type == Material.TRAPPED_CHEST) {
+
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }
